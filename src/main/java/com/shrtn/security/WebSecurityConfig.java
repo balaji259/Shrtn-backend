842ef39ff218922a5ae8ae6d71a55ef5c09602bb
balaji259 <balajipuneti259@gmail.com>
2025-06-29 13:44:22 +0530

    fixed cors issue

--- src/main/java/com/shrtn/security/WebSecurityConfig.java
+++ src/main/java/com/shrtn/security/WebSecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -53,7 +55,9 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/","/api/auth/**").permitAll()
                         .requestMatchers("/api/urls/**").authenticated()
